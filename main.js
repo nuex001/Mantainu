c17317f27c0d946d549bc72ecb6c4343b9fb58d5
nuex001 <edeemmanuelchizurumoke@gmail.com>
2024-01-18 01:03:44 +0100

    new update on the responsive and footer

--- main.js
+++ main.js
@@ -17,12 +17,35 @@ menuBtn.addEventListener("click", toggleMenu)
 
 
 const token = document.querySelector(".token");
+const count = document.querySelector(".count");
+const count2 = document.querySelector(".count2");
 
-//WINDOW EVENT
-window.addEventListener("scroll", () => {
+const handleScroll = () => {
     const tokenTop = token.getBoundingClientRect().top;
-    const triggerbottom = window.innerHeight / 6 * 4;
-    if (tokenTop < triggerbottom) {
-        token.classList.add("active");
+    const triggerbottom = window.innerHeight / 5 * 4;
+    if (tokenTop <= triggerbottom) {
+        const max = 21000000000;
+        let countNo = 0;
+        const max2 = 22;
+        let countNo2 = 0;
+        const interval = setInterval(() => {
+            countNo += 200000000;
+            if (countNo >= max) {
+                clearInterval(interval);
+            }
+            count.innerHTML = countNo.toLocaleString();
+        }, 100); //1miliseconds
+
+        const interval2 = setInterval(() => {
+            countNo2 += 1;
+            if (countNo2 >= max2) {
+                clearInterval(interval2);
+            }
+            count2.innerHTML = countNo2.toLocaleString();
+        }, 100); //1miliseconds
+        // Remove the scroll event listener after executing the code
+        window.removeEventListener("scroll", handleScroll);
     }
-})
+}
+//WINDOW EVENT
+window.addEventListener("scroll", handleScroll)
